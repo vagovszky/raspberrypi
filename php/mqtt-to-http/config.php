1a18e84059d78ea22d5ee453c3ab77b911aeb094
Martin Vagovszky <Martin.Vagovszky@honeywell.com>
2016-11-29 13:35:48 +0100

    Listeners update

--- php/mqtt-to-http/config.php
+++ php/mqtt-to-http/config.php
@@ -7,7 +7,18 @@ return array(
     'mqtt_port' => 1883,
     'mqtt_user' => '',
     'mqtt_password' => '',
-    'mqtt_request_topic' => 'bridge/request',
-    'mqtt_response_topic' => 'bridge/response'
+    'mqtt_request_topic' => 'bridge/command/request',
+    'mqtt_response_topic' => 'bridge/command/response'
+  ),
+  'mqtt_listener' => array(
+    'timeout' => 60,
+    'client_name' => 'php_mqtt_listener'
+    'mqtt_host' => 'localhost'
+    'mqtt_port' => 1883,
+    'mqtt_user' => '',
+    'mqtt_password' => '',
+    'mqtt_topic' => 'bridge/thingspeak/request',
+    'thingspeak_url' => 'http://api.thingspeak.com/update',
+    'thingspeak_api_key' => ''
   )
 );
